4e4e9051dec4b24cd5fe08f71653397e88b49b2d
filauro1989 <filauro1989@gmail.com>
2022-01-13 16:49:19 +0100

    compiling header

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,29 @@
 <template>
   <div id="app">
-    <header />
-    <main />
-    <footer />
+    <Header />
+    <Main />
+    <Footer />
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import header from './components/header.vue'
-import main from './components/main.vue'
-import footer from './components/footer.vue'
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    header,
-    main,
-    footer,
+    Header,
+    Main,
+    Footer,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  @import "./assets/scss/style.scss";
+  
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,100 @@
+<template>
+    <header>
+        <div class="container text-center d-flex bg-green">
+            <div class="logo-container">
+                <img src="../assets/img/dc-logo.png" alt="">
+            </div>
+            <div class="nav-list">
+                <ul>
+                    <li>
+                        <a href="">CHARACTERS</a>
+                    </li>
+                    <li>
+                        <a href="" class="active">COMICS</a>
+                    </li>
+                    <li>
+                        <a href="">MOVIES</a>
+                    </li>
+                    <li>
+                        <a href="">TV</a>
+                    </li>
+                    <li>
+                        <a href="">GAMES</a>
+                    </li>
+                    <li>
+                        <a href="">COLLECTIBLES</a>
+                    </li>
+                    <li>
+                        <a href="">VIDEOS</a>
+                    </li>
+                    <li>
+                        <a href="">FANS</a>
+                    </li>
+                    <li>
+                        <a href="">NEWS</a>
+                    </li>
+                    <li>
+                        <a href="">SHOP</a>
+                    </li>
+                </ul>
+            </div>
+        </div>    
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data() { //data e' una funzione che restituisce il nostro object
+        return {
+            title: "Lorem Ipsum pippo",
+        }
+    }, 
+    methods: {
+        // changeActive(indexElement) {
+        // this.links.forEach(
+        //     (element, index) => {
+        //     if (index === indexElement) {
+        //         element.current = true;
+        //     } else {
+        //         element.current = false;
+        //     }
+        //     }
+        // )
+        // }
+}
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+
+    @import "../assets/scss/partials/_variables.scss";
+
+    .d-flex {
+        align-items: center;
+        justify-content: space-around;
+    }
+    ul {
+        list-style: none;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+            li {
+                padding: 0 0.5em;
+                    a {
+                        display: block;
+                        text-decoration: none;
+                        padding: 2em 0;
+                        border-bottom: 5px solid white;
+                        &:hover,
+                        &:active,
+                        &.active {
+                            color: $fontColor;
+                            border-bottom: 5px solid $fontColor;
+                        }
+                }
+            }
+    }
+</style>
