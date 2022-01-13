cabbc0ab92489d42cbd6f268ee1dddfcf3e0dfe4
filauro1989 <filauro1989@gmail.com>
2022-01-13 17:33:10 +0100

    main completed

--- src/components/main.vue
+++ src/components/main.vue
@@ -5,9 +5,49 @@
                 <p> --> Content goes here  </p>
             </div>
         </div>
-        <div class="bottom">
-            <div class="container">
-
+        <div class="bottom d-flex">
+            <div class="container d-flex space-around">
+                <div class="bottom-box d-flex">
+                    <div class="img-wrap">
+                        <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                    </div>
+                    <div class="text-wrap">
+                        <p>digital comics</p>
+                    </div>
+                </div>
+                <div class="bottom-box d-flex">
+                    <div class="img-wrap">
+                        <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                    </div>
+                    <div class="text-wrap">
+                        <p>digital comics</p>
+                    </div>
+                </div>
+                <div class="bottom-box d-flex">
+                    <div class="img-wrap">
+                        <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+                    </div>
+                    <div class="text-wrap">
+                        <p>digital comics</p>
+                    </div>
+                </div>
+                <div class="bottom-box d-flex">
+                    <div class="img-wrap">
+                        <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                    </div>
+                    <div class="text-wrap">
+                        <p>digital comics</p>
+                    </div>
+                </div>
+                <div class="bottom-box d-flex">
+                    <div class="img-wrap">
+                        <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                    </div>
+                    <div class="text-wrap">
+                        <p>digital comics</p>
+                    </div>
+                </div>
+                
             </div>
         </div>
     </main>
@@ -35,16 +75,28 @@ export default {
     .d-flex {
         align-items: center;
     }
+    .space-around {
+        justify-content: space-around;
+    }
     .top {
         background-color: black;
         height: 150px;
         .container p {       
-            font-size: 1.5em;
+            font-size: 1.3em;
         }
     }
     .bottom {
         background-color: $fontColor;
         height: 150px;
+
+        .img-wrap img {
+            height: 3em;
+        };
+        .text-wrap {
+            font-size: 0.8em;
+            padding: 0 1em;
+            text-transform: uppercase;
+        }
     }
 
 </style>
