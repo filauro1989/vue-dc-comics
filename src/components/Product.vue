c61b84e83f88314d33eefb759f4e029d7e74db4e
filauro1989 <filauro1989@gmail.com>
2022-01-14 15:08:43 +0100

    doing cards

---
+++ src/components/Product.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="card">
+        <div class="product">
+            <img
+                :src="img" 
+                :alt="alt"
+                
+            >
+        </div>
+        <h2>{{ titolo }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Product",
+    props: ['img', 'alt', 'titolo', 'tipo', 'prezzo'],
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .card {
+        display: flex;
+        flex-wrap: wrap;
+        flex-basis: calc((100% / 6) - 0.4em);
+        h2 {
+            font-size: 0.8em;
+            text-transform: uppercase;
+        };
+        .product {
+            margin: 0.2em;
+            height: 150px;
+            width: 150px;
+            overflow: hidden;
+            img {
+            width: 100%;
+            }
+        }
+    }
+</style>
